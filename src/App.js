c2b4444df98c3103285afb00e9f992f71ef943ed
huanyinyueye <lightfugitive@gmail.com>
2021-10-22 14:53:31 +0800

    CSS drop down menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,170 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul class="drop-down-menu">
+      <p>Drop Down Menu!</p>
+      <li>
+        <a href="#">menu 1</a>
+        <ul>
+          <li>
+            <a href="#">menu 1-1</a>
+            <ul>
+              <li>
+                <a href="#">menu 1-1-1</a>
+                <ul>
+                  <li>
+                    <a href="#">menu 1-1-1-1</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">menu 1-1-2</a>
+              </li>
+              <li>
+                <a href="#">menu 1-1-3</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">menu 1-2</a>
+            <ul>
+              <li>
+                <a href="#">menu 1-2-1</a>
+              </li>
+              <li>
+                <a href="#">menu 1-2-2</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">menu 1-3</a>
+          </li>
+          <li>
+            <a href="#">menu 1-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 2</a>
+        <ul>
+          <li>
+            <a href="#">menu 2-1</a>
+          </li>
+          <li>
+            <a href="#">menu 2-2</a>
+          </li>
+          <li>
+            <a href="#">menu 2-3</a>
+          </li>
+          <li>
+            <a href="#">menu 2-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 3</a>
+        <ul>
+          <li>
+            <a href="#">menu 3-1</a>
+          </li>
+          <li>
+            <a href="#">menu 3-2</a>
+          </li>
+          <li>
+            <a href="#">menu 3-3</a>
+          </li>
+          <li>
+            <a href="#">menu 3-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 4</a>
+        <ul>
+          <li>
+            <a href="#">menu 4-1</a>
+          </li>
+          <li>
+            <a href="#">menu 4-2</a>
+          </li>
+          <li>
+            <a href="#">menu 4-3</a>
+          </li>
+          <li>
+            <a href="#">menu 4-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 5</a>
+        <ul>
+          <li>
+            <a href="#">menu 5-1</a>
+          </li>
+          <li>
+            <a href="#">menu 5-2</a>
+          </li>
+          <li>
+            <a href="#">menu 5-3</a>
+          </li>
+          <li>
+            <a href="#">menu 5-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 6</a>
+        <ul>
+          <li>
+            <a href="#">menu6-1</a>
+          </li>
+          <li>
+            <a href="#">menu 6-2</a>
+          </li>
+          <li>
+            <a href="#">menu 6-3</a>
+          </li>
+          <li>
+            <a href="#">menu 6-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 7</a>
+        <ul>
+          <li>
+            <a href="#">menu 7-1</a>
+          </li>
+          <li>
+            <a href="#">menu 7-2</a>
+          </li>
+          <li>
+            <a href="#">menu 7-3</a>
+          </li>
+          <li>
+            <a href="#">menu 7-4</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">menu 8</a>
+        <ul>
+          <li>
+            <a href="#">menu 8-1</a>
+          </li>
+          <li>
+            <a href="#">menu 8-2</a>
+          </li>
+          <li>
+            <a href="#">menu 8-3</a>
+          </li>
+          <li>
+            <a href="#">menu 8-4</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
   );
 }
 
